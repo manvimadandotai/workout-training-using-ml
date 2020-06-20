89b48b454963a7c67c23c2cf05d3dfd2fa5d6fdd
Manvi Madan <manvimadan@outlook.com>
2020-06-20 19:42:57 +1200

    training, testing and confusion matrix

--- algorithm.R
+++ algorithm.R
@@ -36,7 +36,7 @@ inTrain <- caret::createDataPartition(y = data$classe, p = 0.7, list = FALSE)
 # subset
 training <- data[inTrain, ]
 testing <- data[-inTrain, ]
-
+  
 
 #four ggplot boxplots
 p.roll.belt <- ggplot(training, aes(classe, roll_belt))
@@ -45,6 +45,39 @@ p.pitch.belt <- ggplot(training, aes(classe, pitch_belt))
 p.pitch.belt <- p.pitch.belt + geom_boxplot() + ggtitle("Pitch Belt")
 p.yaw.belt <- ggplot(training, aes(classe, yaw_belt))
 p.yaw.belt <- p.yaw.belt + geom_boxplot() + ggtitle("Yaw Belt")
-p.accel.belt <- ggplot(training, aes(classe, total_accel_belt))
+  p.accel.belt <- ggplot(training, aes(classe, total_accel_belt))
 p.accel.belt <- p.accel.belt + geom_boxplot() + ggtitle("Total Accel Belt")
 gridExtra::grid.arrange(p.roll.belt, p.pitch.belt, p.yaw.belt, p.accel.belt, ncol = 2, nrow = 2)
+
+
+# simple decision tree model, isolating the class (60) as the outcome and 
+# variables 8 - 11 as predictors  
+modFit <- caret::train(classe ~ ., method = "rpart", data = training[,c(8:11,60)])
+
+rattle::fancyRpartPlot(modFit$finalModel)
+
+# make sure to use the same variables from the testing set as were used in the training set.  
+predictions <- predict(modFit, newdata = testing[,c(8:11,60)])
+
+# output confusion matrix
+confusionMatrix(predictions, testing$classe)
+
+
+# random forest using all predictors
+
+modFit.rf <- randomForest::randomForest(classe ~ ., data = training[,c(8:60)])
+
+
+
+modFit.rf
+
+
+predictions.rf <- predict(modFit.rf, newdata = testing[,c(8:60)])
+confusionMatrix(predictions.rf, testing$classe)
+
+plot(modFit.rf, main = "Error rate of random forest")
+
+varImpPlot(modFit.rf, pch = 20, main = "Importance of Variables")
+
+
+
